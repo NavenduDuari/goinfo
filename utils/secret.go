00513b4db51ef5d00e4ade2d1c8374d335ea6f14
Navendu Duari <nduari09@gmail.com>
2020-04-09 11:22:45 +0530

    deploying to heroku

---
+++ utils/secret.go
@@ -0,0 +1,7 @@
+package utils
+
+var (
+	TwilioAccountSid = "ACeb0f2578fd7aeb5857b13330989839b8"
+	TwilioAuthToken  = "0fce70ba4ef8ad9eb6d5a80caf7feb4c"
+	NomicsApiKey     = "5cfaeb13f3b4267463d693870250bae7"
+)
