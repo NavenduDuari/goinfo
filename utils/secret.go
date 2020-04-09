ac5feaeb7f765f0023bfe34e6d26b1a23a4d3d62
Navendu Duari <nduari09@gmail.com>
2020-04-10 02:54:37 +0530

    twilio authToken changed

--- utils/secret.go
+++ utils/secret.go
@@ -2,6 +2,6 @@ package utils
 
 var (
 	TwilioAccountSid = "ACeb0f2578fd7aeb5857b13330989839b8"
-	TwilioAuthToken  = "0fce70ba4ef8ad9eb6d5a80caf7feb4c"
+	TwilioAuthToken  = "b7312898f9cbf01e3a2bc9d5e5573d54"
 	NomicsApiKey     = "5cfaeb13f3b4267463d693870250bae7"
 )
